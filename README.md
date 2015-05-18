075815234946596b134a7b7f044efbdfc07425fc
fffw <fffw@getlantern.org>
2015-05-18 19:52:55 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+Following command sequence is tested on an Digital Ocean Ubuntu 14.04 64bit droplet.
+
+```
+sudo apt-get update
+sudo apt-get -y install git gcc npm nodejs
+sudo ln -s /usr/bin/nodejs /usr/bin/node
+
+wget http://get.influxdb.org/influxdb_0.9.0-rc30_amd64.deb
+sudo dpkg -i influxdb_0.9.0-rc30_amd64.deb
+sudo /etc/init.d/influxdb start
+
+wget https://storage.googleapis.com/golang/go1.4.2.linux-amd64.tar.gz
+tar zxvf go1.4.2.linux-amd64.tar.gz
+mkdir ~/wd
+cat - >> .profile <<'EOF'
+export GOROOT=~/go
+export PATH=$GOROOT:$PATH
+export GOPATH=~/wd
+EOF
+source .profile
+
+cd $GOPATH/src/github.com/grafana/grafana
+go run build.go setup
+$GOPATH/bin/godep restore
+
+npm install
+npm install -g grunt-cli
+grunt
+
+go build .
+```
+
+`vi ~/wd/src/github.com/grafana/grafana/conf/defaults.ini`
+Under `[auth.anonymous]`, modify `enabled = true`
+Under `[security]`, Change `admin_user` and `admin_password`
+
+```
+curl -G http://localhost:8086/query --data-urlencode "q=CREATE DATABASE sonar"
+grafana
+```
+
+```
+cd $GOPATH/src/github.com/getlantern/influx-example/
+go run main.go
+```
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"net/url"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/influxdb/influxdb/client"
+)
+
+const (
+	//MyHost = "128.199.154.66"
+	MyHost        = "localhost"
+	MyPort        = 8086
+	MyDB          = "sonar"
+	MyMeasurement = "loadTime"
+)
+
+func main() {
+	rand.Seed(int64(time.Now().Second()))
+	u, err := url.Parse(fmt.Sprintf("http://%s:%d", MyHost, MyPort))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := client.Config{
+		URL:      *u,
+		Username: os.Getenv("INFLUX_USER"),
+		Password: os.Getenv("INFLUX_PWD"),
+	}
+
+	con, err := client.NewClient(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	dur, ver, err := con.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Happy as a Hippo! %v, %s", dur, ver)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, os.Interrupt, os.Kill)
+	tick := time.Tick(500 * time.Millisecond)
+outer:
+	for {
+		select {
+		case <-tick:
+			writePoints(con)
+		case <-sigs:
+			break outer
+		}
+	}
+	printMeasures()
+}
+
+func writePoints(con *client.Client) {
+	var (
+		countries  = []string{"Iran", "China", "Syria", "Philippines"}
+		versions   = []string{"1.5.17", "2.0.0-beta4", "2.0.0-beta5"}
+		sites      = []string{"Twitter", "Facebook", "Google", "Baidu"}
+		sampleSize = 10000
+		pts        = make([]client.Point, sampleSize)
+	)
+
+	for i := 0; i < sampleSize; i++ {
+		pts[i] = client.Point{
+			Name: "sonar",
+			Tags: map[string]string{
+				"site":    sites[rand.Intn(len(sites))],
+				"version": versions[rand.Intn(len(versions))],
+				"country": countries[rand.Intn(len(countries))],
+			},
+			Fields: map[string]interface{}{
+				"loadTime": time.Duration((10000 + rand.Intn(1000))) * time.Millisecond,
+			},
+			Time:      time.Now(),
+			Precision: "s",
+		}
+	}
+	bps := client.BatchPoints{
+		Points:          pts,
+		Database:        MyDB,
+		RetentionPolicy: "default",
+	}
+	start := time.Now()
+	_, err := con.Write(bps)
+	if err != nil {
+		log.Fatalf("Write error: %s", err)
+	}
+	log.Printf("Write %d data points, takes %v", sampleSize, time.Now().Sub(start))
+}
+
+func printMeasures() {
+}
